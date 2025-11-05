d9a9a8ad80502797da11f1dae29961933cbb34f6
SanGianniiii <p.sangiovanniapostolocatania@gmail.com>
2025-11-05 10:01:40 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,26 @@
-const CACHE_NAME = 'ordini-post-v2'; // Ho incrementato la versione per assicurare l'aggiornamento
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/logo.png',
-  '/manifest.json',
-  // Aggiungi qui altri file statici che vuoi mettere in cache
-];
-
 self.addEventListener('install', function(e) {
-  console.log('[SW] Installazione Service Worker');
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache) {
-      console.log('[SW] Caching assets');
-      return cache.addAll(urlsToCache);
-    }).then(() => self.skipWaiting()) // Attiva il nuovo SW immediatamente
-  );
-});
-
-self.addEventListener('activate', function(e) {
-  console.log('[SW] Attivazione Service Worker');
-  e.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('[SW] Eliminazione vecchia cache', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim()) // Permette al nuovo SW di prendere il controllo immediatamente
-  );
+  e.waitUntil(
+    caches.open('ordini-post').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/logo.png',
+        '/manifest.json'
+      ]);
+    })
+  );
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
-
 self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/') 
-  );
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('/') // oppure pagina specifica
+  );
 });
