56897d6d3e5df6296a4aa7a88a350ec6f752c539
SanGianniiii <p.sangiovanniapostolocatania@gmail.com>
2026-09-03 01:48:49 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -14,9 +14,9 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function(payload) {
-  const notificationTitle = payload.notification.title;
+  const notificationTitle = payload.data.title;
   const notificationOptions = {
-    body: payload.notification.body,
+    body: payload.data.body,
     icon: '/logo.png'
   };
   self.registration.showNotification(notificationTitle, notificationOptions);
